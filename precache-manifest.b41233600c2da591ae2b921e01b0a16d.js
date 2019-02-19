59ed9be712a35e2c6a032913cb7f4adf9e81e5da
Zach <zachary.kramer@syneoshealth.com>
2019-02-19 15:45:37 -0500

    Updates

---
+++ precache-manifest.b41233600c2da591ae2b921e01b0a16d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "16f15c243d81d0aeb2842c92b269845b",
+    "url": "/navicor-dragdrop-game/static/media/life.16f15c24.png"
+  },
+  {
+    "revision": "c3ecce4c5f1bd1de5a5561784a0c5d02",
+    "url": "/navicor-dragdrop-game/static/media/life-lost.c3ecce4c.png"
+  },
+  {
+    "revision": "23004ccb06a44241dca7",
+    "url": "/navicor-dragdrop-game/static/js/runtime~main.23004ccb.js"
+  },
+  {
+    "revision": "d3d33713c24ea4be05ff",
+    "url": "/navicor-dragdrop-game/static/js/main.d3d33713.chunk.js"
+  },
+  {
+    "revision": "74f4081947f747ab5e93",
+    "url": "/navicor-dragdrop-game/static/js/2.74f40819.chunk.js"
+  },
+  {
+    "revision": "d3d33713c24ea4be05ff",
+    "url": "/navicor-dragdrop-game/static/css/main.9f132f26.chunk.css"
+  },
+  {
+    "revision": "61b44170bf7799c82ccbc5d854476823",
+    "url": "/navicor-dragdrop-game/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/navicor-dragdrop-game/precache-manifest.b41233600c2da591ae2b921e01b0a16d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/navicor-dragdrop-game/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
